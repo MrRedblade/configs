1d5bd077e0890c3592f53f87fb61d53b99c549d7
MrRedblade <49073997+MrRedblade@users.noreply.github.com>
2022-04-08 17:31:19 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo -ne "
 Welcome to Clancy's shithole configs. Time to install shit!"
 
 sudo apt update && apt upgrade
-sudo apt install firefox kdenlive ckb-next libreoffice piper
+sudo apt -y install firefox kdenlive ckb-next libreoffice piper
 sudo cp update.sh /usr/local/bin/
 sudo chmod +x /usr/local/bin/update.sh
 
